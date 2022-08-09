c4de04930fd0311b59d448d8e51bceb642e6bf7d
Crash <crash555@mail.ru>
2022-08-09 15:49:41 +0500

    Making Six Toast

---
+++ Making Six Toast.js
@@ -0,0 +1,9 @@
+function sixToast(num) {
+    var ostatokToast;
+    if (num < 6) {
+      return ostatokToast = 6 - num;
+    } else {
+      return ostatokToast = num - 6;
+    }
+      return ostatokToast;
+}
